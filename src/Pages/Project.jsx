d19fb0e6c98e4cd99a036059036757a39b6bbf3e
neema34 <21b34.neema@sjec.ac.in>
2025-02-24 18:11:12 +0530

    first commit

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -4,8 +4,10 @@ import proj2 from "/Users/NEEMA/Downloads/Royalheritage (2)/Royalheritage/src/as
 import proj3 from "/Users/NEEMA/Downloads/Royalheritage (2)/Royalheritage/src/assets/proj3.jpg";
 import proj4 from "/Users/NEEMA/Downloads/Royalheritage (2)/Royalheritage/src/assets/proj4.jpg";
 import proj5 from "/Users/NEEMA/Downloads/Royalheritage (2)/Royalheritage/src/assets/proj5.jpg";
-import proj1 from "../../assets/proj1.jpg";
-import header from "../../assets/header.jpg";
+import proj1 from "/Users/NEEMA/Downloads/Royalheritage (2)/Royalheritage/src/assets/proj1.jpg";
+import header from "/Users/NEEMA/Downloads/Royalheritage (2)/Royalheritage/src/assets/header.jpg";
+
+
 
 
 const images = [
@@ -29,7 +31,7 @@ const Header = () => {
           <div
             key={index}
             className={`slide ${index === currentIndex ? "active" : "inactive"}`}
-            style={{ backgroundImage: `url(${img})` }}
+            style={{ backgroundImage: `url(${images[currentIndex]})` }}
             role="img"
             aria-label={`Slide ${index + 1}`}
           />
