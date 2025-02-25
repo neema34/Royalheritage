6656672afd1c890d6099f7f6aefac71a90281bfe
neema34 <21b34.neema@sjec.ac.in>
2025-02-25 14:46:29 +0530

    whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
     <Router>
-    <WhatsAppButton /> 
+    <Whatsapp/> 
       <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='about/' element={<About/>}/>
